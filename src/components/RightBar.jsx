c6fb2f6eab699825e7f7b956602fdf4e7b08b64a
sucwin <taipv2x@gmail.com>
2022-01-11 11:34:35 +0700

    logic menu tab

--- src/components/RightBar.jsx
+++ src/components/RightBar.jsx
@@ -1,19 +1,27 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Avatar from './Avatar';
 import Tab from './Tab'
 import Todaytab from './TodayTab/TodayTab';
 import WeekTab from './WeekTab/WeekTab';
 import Hour from './Hour/Hour'
 const Rightbar = () => {
+  const [tab, setTab]= useState('today')
+  
+  const render= tab==='today'? 
+    <Todaytab/>: (tab==='week'? 
+    <WeekTab/> : 
+    <Hour/> )
+
+  const handleChangeTab=(tabName)=>{
+    setTab(tabName)
+  }
   return (
     <div className="wrap">
       <div className="d-flex align-item-center justify-content-between">
-        <Tab />
+        <Tab onChangeTab={handleChangeTab} currentTab={tab} />
         <Avatar />
       </div>
-      {/* <Todaytab/> */}
-      {/* <WeekTab/> */}
-      <Hour/>
+      {render}
     </div>
 
   );
--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -1,18 +1,34 @@
 import React from 'react';
 
-const Tab = () => {
+const Tab = ({onChangeTab, currentTab}) => {
+  
     return (
     <ul className="nav d-flex align-item-center justify-content-start fs-5 fw-bold">
       <li
         role="button"
-        className="nav-item m-2 border-bottom border-3 border-dark"
+        className={currentTab==='today'? 
+        "nav-item m-2 border-bottom border-3 border-dark": "nav-item m-2 text-muted"
+        }
+        onClick={()=>onChangeTab('today')}
       >
         Today
       </li>
-      <li role="button" className="nav-item m-2 text-muted">
+      <li 
+        role="button"
+        className={currentTab==='week'? 
+        "nav-item m-2 border-bottom border-3 border-dark": "nav-item m-2 text-muted"
+        }
+        onClick={()=>onChangeTab('week')}
+      >
         Week
       </li>
-      <li role="button" className="nav-item m-2 text-muted">
+      <li 
+        role="button" 
+        className={currentTab==='hour'? 
+        "nav-item m-2 border-bottom border-3 border-dark": "nav-item m-2 text-muted"
+        }
+        onClick={()=>onChangeTab('hour')}
+      >
         Hour
       </li>
     </ul>
