69371f56c4276f0f5a15865b8623ad40bab4a064
sucwin <taipv2x@gmail.com>
2022-01-12 19:46:15 +0700

    done

--- src/components/RightBar.jsx
+++ src/components/RightBar.jsx
@@ -3,10 +3,12 @@ import Avatar from './Avatar';
 import Tab from './Tab'
 import Todaytab from './TodayTab/TodayTab';
 import WeekTab from './WeekTab/WeekTab';
-import Hour from './Hour/Hour'
+import Hour from './Hour/Hour';
+import {useSelector} from 'react-redux';
+
 const Rightbar = () => {
   const [tab, setTab]= useState('today')
-  
+  const weatherReducer = useSelector(state => state.weatherReducer);
   const render= tab==='today'? 
     <Todaytab/>: (tab==='week'? 
     <WeekTab/> : 
@@ -15,15 +17,19 @@ const Rightbar = () => {
   const handleChangeTab=(tabName)=>{
     setTab(tabName)
   }
+
   return (
     <div className="wrap">
+      {
+        weatherReducer.error===true && 
+        <div className="alert alert-danger" role="alert">Not found city</div>
+      }
       <div className="d-flex align-item-center justify-content-between">
         <Tab onChangeTab={handleChangeTab} currentTab={tab} />
         <Avatar />
       </div>
       {render}
     </div>
-
   );
 }
 
--- src/store/reducers/WeatherSlice.js
+++ src/store/reducers/WeatherSlice.js
@@ -3,12 +3,17 @@ import axios from 'axios';
 import {APPID} from '../../config'
 export const getWeatherAsync = createAsyncThunk(
   'weather/weatherFetch',
-  async (province)=>{
-        const location= await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${province}&units=metric&APPID=${APPID}`);
+  async (province, { rejectWithValue })=>{
+    try {
+      const location= await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${province}&units=metric&APPID=${APPID}`);
         const {lat, lon} =location.data.coord
         const resGetAsync= await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&APPID=e5f1e0e91073e047bfd37039ad433153`);
         const weather={...resGetAsync.data, name:location.data.name}
         return  weather
+    } catch (err) {
+       return rejectWithValue(err.response.data);
+    }
+        
   }
 )
 
